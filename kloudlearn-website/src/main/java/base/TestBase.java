4948a45c6d012cb4ec8e0c1360df318281119e47
suman-s-dhal <64960704+suman-s-dhal@users.noreply.github.com>
2020-09-28 00:08:08 +0530

    Update TestBase.java
    committed for selenium

--- kloudlearn-website/src/main/java/base/TestBase.java
+++ kloudlearn-website/src/main/java/base/TestBase.java
@@ -18,6 +18,7 @@ public class TestBase {
 	    	
 	    	ChromeOptions options = new ChromeOptions();
 		options.addArguments("--no-sandbox");
+		options.addArguments("--headless");
 		options.addArguments("--disable-dev-shm-usage");
 	    	options.addArguments("use-fake-ui-for-media-stream");
 	    	System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY, "true");
