746efd01e9981b91a1c9b7a1bdaa5b39ed13c1de
suman-s-dhal <64960704+suman-s-dhal@users.noreply.github.com>
2020-09-27 23:11:31 +0530

    Update TestBase.java
    committed for selenium test

--- kloudlearn-website/src/main/java/base/TestBase.java
+++ kloudlearn-website/src/main/java/base/TestBase.java
@@ -19,7 +19,7 @@ public class TestBase {
 	    	ChromeOptions options = new ChromeOptions();
 	    	options.addArguments("use-fake-ui-for-media-stream");
 	    	System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY, "true");
-	    	System.setProperty("webdriver.chrome.driver", "/root/chromedriver");
+	    	System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
 	        browser = new ChromeDriver(options);
 	        browser.manage().window().maximize();
 	        browser.get("https://app.staging-env.kloudlearn.com/auth/login");
