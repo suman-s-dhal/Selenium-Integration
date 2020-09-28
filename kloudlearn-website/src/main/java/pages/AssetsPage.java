1679a021fb681ab0ad171b9f6a34347f748b7d9f
suman-s-dhal <64960704+suman-s-dhal@users.noreply.github.com>
2020-09-28 23:13:20 +0530

    Update AssetsPage.java
    committed for selenium

--- kloudlearn-website/src/main/java/pages/AssetsPage.java
+++ kloudlearn-website/src/main/java/pages/AssetsPage.java
@@ -28,6 +28,7 @@ import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 //import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import java.util.concurrent.TimeUnit;
 
 import base.TestBase;
 
