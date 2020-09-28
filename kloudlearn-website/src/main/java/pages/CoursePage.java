0a27dcf287ed7fa9eff6742f955311d07ca05cdd
suman-s-dhal <64960704+suman-s-dhal@users.noreply.github.com>
2020-09-28 23:14:00 +0530

    Update CoursePage.java
    committed for selenium

--- kloudlearn-website/src/main/java/pages/CoursePage.java
+++ kloudlearn-website/src/main/java/pages/CoursePage.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 //import org.openqa.selenium.support.ui.WebDriverWait;
+import java.util.concurrent.TimeUnit;
 
 import base.TestBase;
 
